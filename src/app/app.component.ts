2c28031f9902b4dea18fb3b148935cecdd05ed33
eduardovscruuz <eduardovscruuz@gmail.com>
2024-12-05 14:27:24 -0300

    movimento das colunas adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,55 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ColumnComponent } from './components/column/column.component';
-import { ContentHomeComponent } from './components/content-home/content-home.component';
-import { ContentAboutComponent } from './components/content-about/content-about.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    ColumnComponent,
-    ContentHomeComponent,
-    ContentAboutComponent,
-  ],
+  imports: [RouterOutlet, ColumnComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio-angular';
+  isHomeActive = true; // Inicialmente ativa a coluna "home"
+  isAboutActive = false;
+  isSkillsActive = false;
+  isProjectsActive = false;
+  isContactActive = false;
+
+  revealHome(): void {
+    this.isHomeActive = true;
+    this.isAboutActive = false;
+    this.isSkillsActive = false;
+    this.isProjectsActive = false;
+    this.isContactActive = false;
+  }
+  revealAbout(): void {
+    this.isHomeActive = false;
+    this.isAboutActive = true;
+    this.isSkillsActive = false;
+    this.isProjectsActive = false;
+    this.isContactActive = false;
+  }
+  revealSkills(): void {
+    this.isHomeActive = false;
+    this.isAboutActive = false;
+    this.isSkillsActive = true;
+    this.isProjectsActive = false;
+    this.isContactActive = false;
+  }
+  revealProjects(): void {
+    this.isHomeActive = false;
+    this.isAboutActive = false;
+    this.isSkillsActive = false;
+    this.isProjectsActive = true;
+    this.isContactActive = false;
+  }
+  revealContact(): void {
+    this.isHomeActive = false;
+    this.isAboutActive = false;
+    this.isSkillsActive = false;
+    this.isProjectsActive = false;
+    this.isContactActive = true;
+  }
 }
--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-column',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './column.component.html',
   styleUrl: './column.component.scss',
 })
 export class ColumnComponent {
   @Input() icon: string = '';
+  @Input() additionalClasses!: string; // Recebe classes dinamicamente
 }
