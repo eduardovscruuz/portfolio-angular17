a780d8d43af9670cc6c154ce67a793c2969fa897
eduardovscruuz <eduardovscruuz@gmail.com>
2024-12-16 20:40:16 -0300

    Downgrade angular v18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ColumnComponent } from './components/column/column.component';
-import { CommonModule } from '@angular/common';
-import { ContentComponent } from './components/content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ColumnComponent, CommonModule, ContentComponent],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio-angular';
-  colorHome: string = '#61e4d5';
-  colorAbout: string = '#57d5e0';
-  colorSkills: string = '#4dc5ea';
-  colorProjects: string = '#43b5f5';
-  colorContact: string = '#38a5ff';
-
-  isHomeActive = true; // Inicialmente ativa a coluna "home"
-  isAboutActive = false;
-  isSkillsActive = false;
-  isProjectsActive = false;
-  isContactActive = false;
-  titulo: string = '';
-
-  revealHome(): void {
-    this.isHomeActive = true;
-    this.isAboutActive = false;
-    this.isSkillsActive = false;
-    this.isProjectsActive = false;
-    this.isContactActive = false;
-  }
-  revealAbout(): void {
-    this.isHomeActive = false;
-    this.isAboutActive = true;
-    this.isSkillsActive = false;
-    this.isProjectsActive = false;
-    this.isContactActive = false;
-  }
-  revealSkills(): void {
-    this.isHomeActive = false;
-    this.isAboutActive = false;
-    this.isSkillsActive = true;
-    this.isProjectsActive = false;
-    this.isContactActive = false;
-  }
-  revealProjects(): void {
-    this.isHomeActive = false;
-    this.isAboutActive = false;
-    this.isSkillsActive = false;
-    this.isProjectsActive = true;
-    this.isContactActive = false;
-  }
-  revealContact(): void {
-    this.isHomeActive = false;
-    this.isAboutActive = false;
-    this.isSkillsActive = false;
-    this.isProjectsActive = false;
-    this.isContactActive = true;
-  }
-
-  columnFocused() {
-    console.log(this.titulo);
-  }
-
-  columnUnfocused() {
-    console.log('desfocou');
-  }
+  title = 'portfolio-angular18';
 }
